85e9db5ef2a7365130b05af75f20fbde229bd96f
joshuashiing <31521175+joshuashiing@users.noreply.github.com>
2017-12-01 02:06:56 -0500

    regular update

---
+++ k-Wave/GX_examples/coef_analysis/test_cubic.py
@@ -0,0 +1,17 @@
+from sympy import *
+x, z, A1, A2, A3, A4, A5, A6 = symbols('x, z, A1, A2, A3, A4, A5, A6')
+c_z0 = -(1 + x)**2
+c_z1 = A1 + I * A4 * (1 + x)
+c_z2 = A2 + I * A5 * (1 + x)
+c_z3 = A3 + I * A6 * (1 + x)
+eqn = c_z0 + c_z1 * z + c_z2 * z**2 + c_z3 * z**3
+z_sol = solve(eqn, z)
+z1_sol = z_sol[0]
+z2_sol = z_sol[1]
+z3_sol = z_sol[2]
+z1_te = series(z1_sol, x)
+print(z1_sol)
+print(z1_te)
+# print(series(z1_sol))
+print('abc')
+
--- k-Wave/GX_examples/dispersion_test_FD.m
+++ k-Wave/GX_examples/dispersion_test_FD.m
@@ -11,13 +11,13 @@ clear;
 f0_m = 200;     % Reference frequency of the medium
 c0_m = 2089;    % Reference phase velocity
 rho = 2200;
-Q   = 1000;
-f0 = 30;       % Reference frequency for calculation
+Q   = 32.5;
+f0 = 300;       % Reference frequency for calculation
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Set up the frequency of interest
 nf  = 100;
-f   = linspace(10, 60, nf);
+f   = linspace(10, 600, nf);
 % f = 40;
 w   = 2 * pi * f;
 
@@ -65,14 +65,15 @@ alpha = w ./ cp * tan(pi * gamma / 2);
 
 mod_mech = 'PF17';
 
-m_max = 2;
+m_max = 4;
 n_max = m_max;
-n_x_n = 2;
+n_x_n = 4;
 
 x = (w - w0) ./ w0;
 x_n_vec = transpose(linspace(-1, 1, n_x_n + 1));
 x_n_vec = x_n_vec(2:end);
-x_n_vec = [-0.5; 1];
+x_n_vec = transpose(linspace(10, 600, n_x_n)) / f0 - 1;
+% x_n_vec = [-0.5; 1];
 
 m_vec = 1 : m_max;
 n_vec = 1 : n_max;
@@ -104,115 +105,129 @@ for i_x_n = 1 : n_x_n
 end
 
 rhs_node = [((1 + x_n_vec).^2); zeros(size(x_n_vec))];
+% cd_vec = (B' * B) \ (B' * rhs_node);
 cd_vec = B \ rhs_node;
 
-c_vec = cd_vec(1 : m_max);
-d_vec = cd_vec((m_max+1) : end);
+c_vec0 = cd_vec(1 : m_max);
+d_vec0 = cd_vec((m_max+1) : end);
 
 lhs_x = (1 + x).^2;
-rhs_x = zeros(size(x));;
+rhs_x = zeros(size(x));
+
 for m = 1 : m_max
-    cm = c_vec(m);
+    cm = c_vec0(m);
     rhs_x = rhs_x + cm * (1+x).^(2*m*(1-gamma)) * exp(1i*(-m*pi*gamma));
 end
 for n = 1 : n_max
-    dn = d_vec(n);
+    dn = d_vec0(n);
     rhs_x = rhs_x + dn * (1+x).^(2*n*(1-gamma)+1) * exp(1i*(pi/2-n*pi*gamma));
 end
 
-c_vec = c_vec .* w0.^(2-2*m_vec') .* c.^(2*m_vec');
-d_vec = d_vec .* w0.^(1-2*n_vec') .* c.^(2*n_vec');
+c_vec = c_vec0 .* w0.^(2-2*m_vec') .* c.^(2*m_vec');
+d_vec = d_vec0 .* w0.^(1-2*n_vec') .* c.^(2*n_vec');
 
 c_k1 = c_vec(1);
-c_k2 = c_vec(2);
 d_k1 = d_vec(1);
+c_k2 = c_vec(2);
 d_k2 = d_vec(2);
+c_k3 = c_vec(3);
+d_k3 = d_vec(3);
+c_k4 = c_vec(4);
+d_k4 = d_vec(4);
 
-w2_rhs = zeros(4, length(x));
-w2_rhs(1, :) = c_k1 * k.^2;
-w2_rhs(2, :) = c_k2 * k.^4;
-w2_rhs(3, :) = d_k1 * (1i*w) .* k.^2;
-w2_rhs(4, :) = d_k2 * (1i*w) .* k.^4;
-w2_rhs = sum(w2_rhs);
+% w2_rhs = zeros(4, length(x));
+% w2_rhs(1, :) = c_k1 * k.^2;
+% w2_rhs(2, :) = d_k1 * (1i*w) .* k.^2;
+% w2_rhs(3, :) = c_k2 * k.^4;
+% w2_rhs(4, :) = d_k2 * (1i*w) .* k.^4;
+% w2_rhs(5, :) = c_k3 * k.^6;
+% w2_rhs(6, :) = d_k3 * (1i*w) .* k.^6;
+% w2_rhs = sum(w2_rhs);
 
 
-% lhs_w2 = w.^2;
-% % rhs_w2 = 
-% 
-% 
-% % Solve the dispersion relation
-% k_sol = zeros(size(w));
-% 
-% % c_k1 = c_vec(1);
-% % c_k2 = c_vec(2);
-% % d_k1 = d_vec(1);
-% % d_k2 = d_vec(2);
-% 
-% for i = 1 : nf
-%     i
-%     w_i = w(i);
-%     lhs = w_i ^ 2;
-%     
-%     syms kk;
+% Solve the dispersion relation
+k_sol = zeros(size(w));
+
+% c_k1 = c_vec(1);
+% c_k2 = c_vec(2);
+% d_k1 = d_vec(1);
+% d_k2 = d_vec(2);
+
+for i = 1 : nf
+    i
+    w_i = w(i);
+    lhs = w_i ^ 2;
+    
+%     % test
+%     k_i = k(i);
+%     lhs = c_k1 * k_i^2 + c_k2 * k_i^4 + ...
+%         d_k1 * (1i*w_i) * k_i^2 + d_k2 * (1i*w_i) * k_i^4;
+%     % test
+    
+    syms kk;
+    eqn = (lhs == c_k1 * kk^2 + c_k2 * kk^4 + c_k3 * kk^6 + c_k4 * kk^8 + ...
+        d_k1 * (1i*w_i) * kk^2 + d_k2 * (1i*w_i) * kk^4 + d_k3 * (1i*w_i) * kk^6) + d_k4 * (1i*w_i) * kk^8;
+%     eqn = (lhs == c_k1 * kk^2 + c_k2 * kk^4 + c_k3 * kk^6 + ...
+%         d_k1 * (1i*w_i) * kk^2 + d_k2 * (1i*w_i) * kk^4 + d_k3 * (1i*w_i) * kk^6);
 %     eqn = (lhs == c_k1 * kk^2 + c_k2 * kk^4 + ...
 %         d_k1 * (1i*w_i) * kk^2 + d_k2 * (1i*w_i) * kk^4);
-% %     eqn = (lhs == C_k1 * kk^2 + C_k2 * kk^4 + C_k3 * kk^6 + + C_k4 * kk^8 + ...
-% %         C_k5 * (1i*w_i) * kk^2 + C_k6 * (1i*w_i) * kk^4 + C_k7 * (1i*w_i) * kk^6 + C_k8 * (1i*w_i) * kk^8);
-% %     eqn = (lhs == C_k1 * kk + C_k2 * kk^2 + C_k3 * kk^3 + ...
-% %         C_k4 * (1i*w_i) * kk + C_k5 * (1i*w_i) * kk^2 + C_k3 * (1i*w_i) * kk^3);
-%     k_root = vpasolve(eqn, kk, k(i));
-%     k_root = double(k_root);
-%     [~, i_root] = min(abs(k_root - k(i)));
-% %     [~, i_root] = min(abs(k_root - k(i).^2));
-%     k_sol(i) = k_root(i_root);
-% %     k_sol(i) = sqrt(k_root(i_root));
-% end
-% cp_sol = w ./ real(k_sol);
-% alpha_sol = -imag(k_sol);
+%     eqn = (lhs == C_k1 * kk^2 + C_k2 * kk^4 + C_k3 * kk^6 + + C_k4 * kk^8 + ...
+%         C_k5 * (1i*w_i) * kk^2 + C_k6 * (1i*w_i) * kk^4 + C_k7 * (1i*w_i) * kk^6 + C_k8 * (1i*w_i) * kk^8);
+%     eqn = (lhs == C_k1 * kk + C_k2 * kk^2 + C_k3 * kk^3 + ...
+%         C_k4 * (1i*w_i) * kk + C_k5 * (1i*w_i) * kk^2 + C_k3 * (1i*w_i) * kk^3);
+    k_root = vpasolve(eqn, kk, k(i));
+    k_root = double(k_root);
+    [~, i_root] = min(abs(k_root - k(i)));
+%     [~, i_root] = min(abs(k_root - k(i).^2));
+    k_sol(i) = k_root(i_root);
+%     k_sol(i) = sqrt(k_root(i_root));
+end
+cp_sol = w ./ real(k_sol);
+alpha_sol = -imag(k_sol);
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % figure;
 
-% Test for two values
-val1 = w.^2;
-val2 = abs(w2_rhs);
-subplot(211);
-plot(f, val1, 'k', 'linewidth', 2); hold on;
-plot(f, val2, 'r--', 'linewidth', 2);
-subplot(212);
-plot(f, (val2 - val1) ./ val1 * 100, 'r', 'linewidth', 2);
+% % Test for two values
+% val1 = w.^2;
+% val2 = abs(w2_rhs);
+% subplot(211);
+% plot(f, val1, 'k', 'linewidth', 2); hold on;
+% plot(f, val2, 'r--', 'linewidth', 2);
+% subplot(212);
+% plot(f, (val2 - val1) ./ val1 * 100, 'r', 'linewidth', 2);
 
 
 
-% % Test for two groups of real values
-% val_a1 = cp;
-% val_a2 = cp_sol;
-% val_b1 = alpha;
-% val_b2 = alpha_sol;
-% subplot(221);
-% plot(f, val_a1, 'k', 'linewidth', 2); hold on;
-% plot(f, val_a2, 'r--', 'linewidth', 2);
-% ylabel('Cp (m/s)')
-% set(gca, 'fontsize', 14);
-% title(['Q = ', num2str(Q), ' | f0 = ', num2str(f0), 'Hz'], 'fontsize', 14); 
-% 
-% subplot(223);
-% plot(f, (val_a2 - val_a1) ./ val_a1 * 100, 'r', 'linewidth', 2);
-% ylabel('Cp Discrep. (%)')
-% xlabel('Frequency (Hz)');
-% set(gca, 'fontsize', 14);
-% 
-% subplot(222);
-% plot(f, val_b1, 'k', 'linewidth', 2); hold on;
-% plot(f, val_b2, 'r--', 'linewidth', 2);
-% ylabel('Alpha (m/s)')
-% set(gca, 'fontsize', 14);
-% title(['Red Dashed Line: ', mod_mech], 'fontsize', 14); 
-% 
-% subplot(224);
-% plot(f, (val_b2 - val_b1) ./ val_b1 * 100, 'r', 'linewidth', 2);
-% ylabel('Alpha Discrep. (%)')
-% xlabel('Frequency (Hz)');
-% set(gca, 'fontsize', 14);
+% Test for two groups of real values
+val_a1 = cp;
+val_a2 = cp_sol;
+val_b1 = alpha;
+val_b2 = alpha_sol;
+subplot(221);
+plot(f, val_a1, 'k', 'linewidth', 2); hold on;
+plot(f, val_a2, 'r--', 'linewidth', 2);
+ylabel('Cp (m/s)')
+set(gca, 'fontsize', 14);
+title(['Q = ', num2str(Q), ' | f0 = ', num2str(f0), 'Hz'], 'fontsize', 14); 
+
+subplot(223);
+plot(f, (val_a2 - val_a1) ./ val_a1 * 100, 'r', 'linewidth', 2);
+ylabel('Cp Discrep. (%)')
+xlabel('Frequency (Hz)');
+set(gca, 'fontsize', 14);
+
+subplot(222);
+plot(f, val_b1, 'k', 'linewidth', 2); hold on;
+plot(f, val_b2, 'r--', 'linewidth', 2);
+ylabel('Alpha (m/s)')
+set(gca, 'fontsize', 14);
+title(['Red Dashed Line: ', mod_mech], 'fontsize', 14); 
+
+subplot(224);
+plot(f, (val_b2 - val_b1) ./ val_b1 * 100, 'r', 'linewidth', 2);
+ylabel('Alpha Discrep. (%)')
+xlabel('Frequency (Hz)');
+set(gca, 'fontsize', 14);
 
